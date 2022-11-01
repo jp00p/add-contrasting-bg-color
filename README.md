c748396ac72d1626f77d9d52372d9e54f58d0d1a
Jpop Wilson <asimovsghost@gmail.com>
2022-11-01 07:53:21 -0700

    removing colorthief

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # add-contasting-bg-color
- takes a batch of transparent PNG images, finds the dominant color and adds the inverse of that color as a background.  saves images as JPGs
+ takes a batch of transparent PNG images, finds the dominant color and adds the inverse of that color as a background
+
+## instructions
+- put your images in `input_files`
+- run the script via `python main.py` or however you like
+- enjoy the new images in `output_files`
+
 
 Example input             |  Example output
 :-------------------------:|:-------------------------:
--- main.py
+++ main.py
@@ -1,25 +1,44 @@
-import os, glob
-from PIL import Image, ImageOps
-from colorthief import ColorThief
-
+import os, glob, random
+from PIL import Image, ImageOps, ImageFilter, ImageEnhance
 
 input_dir = "input_images"
 output_dir = "output_images"
 
 for filepath in glob.iglob(f"{input_dir}/*.png"):
   if filepath.endswith(".png"):
-    filename = filepath.lower().replace("/", "").replace("\\", "").replace(input_dir, "").replace(".png", ".jpg")
+    filename = filepath.lower().replace("/", "").replace("\\", "").replace(input_dir, "")
     print(f"Converting {filepath}...")
-    color_thief = ColorThief(filepath)
-    dominant_color = color_thief.get_color(quality=1)
-    
+    random_color = (random.randint(45,255), random.randint(45,255), random.randint(45,255))
     image = Image.open(filepath).convert("RGBA")
-    
-    new_image = Image.new("RGB", image.size, dominant_color) # background color image
+
+    '''
+    uncomment to replace a color (RGB) with transparency
+    you can also do >= comparison on the color if you want to get fuzzy
+    '''
+    # pixdata = image.load()
+    # width, height = image.size
+    # for y in range(height):
+    #     for x in range(width):
+    #         if pixdata[x, y] == (0, 0, 0, 255):
+    #             pixdata[x, y] = (255, 255, 255, 0)
+
+
+    '''uncomment to crop image to the center'''
+    # width, height = image.size
+    # new_size = 64
+    # left = (width - new_size)/2
+    # top = (height - new_size)/2
+    # right = (width + new_size)/2
+    # bottom = (height + new_size)/2
+    # image = image.crop((left, top, right, bottom))
+ 
+
+    image = image.resize((512,512), Image.NEAREST)
+    new_image = Image.new("RGB", image.size, random_color)
     new_image = ImageOps.invert(new_image)
     new_image = new_image.convert("RGBA")
-    new_image.paste(image, (0, 0), image)              # Paste the image on the background. Go to the links given below for details.
-    new_image.convert('RGB').save(f'{output_dir}/{filename}', "JPEG")  # Save as JPEG
+    new_image.paste(image, (0, 0), image)
+    new_image.convert('RGB').save(f'{output_dir}/{filename}', "PNG")
     print(f"Converted {filename}!")
 print("Processing complete!")
 exit()
